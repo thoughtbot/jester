f8922ee7ce17fe1093644c046e5c59d901646a23
emill <emill@d7758119-aa2c-0410-afcd-b700fbd0d0b3>
2007-04-13 19:40:02 +0000

    Prettied up function layout, slightly reordered functions.
    git-svn-id: https://svn.thoughtbot.com/jester/trunk@37 d7758119-aa2c-0410-afcd-b700fbd0d0b3

--- jester.js
+++ jester.js
@@ -83,345 +83,15 @@ function Base(name, prefix, singular, plural) {
   this._tree.attr_prefix = "@";
 }
 
+// Idea taken from Prototype
+extend = function(object, properties) {
+  for (var property in properties) {
+    object[property] = properties[property];
+  }
+}
+
 // Model declaration helper
 Base.model = function(name, prefix, singular, plural) {eval(name + " = new Base(name, prefix, singular, plural);")}
-
-// helper URLs
-Base.prototype.singular_url = function(id) {return ((id || this.id) ? this._prefix + "/" + this._plural + "/" + (id || this.id) + ".xml" : "");}
-Base.prototype.plural_url = function() {return this._prefix + "/" + this._plural + ".xml";}
-
-// And a record shall be judged new or old by its ID
-Base.prototype.new_record = function() {return !(this.id);}
-
-// Validation helper
-Base.prototype.valid = function() {return ! this.errors.any();}
-
-// Find by ID
-Base.prototype.find = function(id, callback) {
-  findAllTransform = function(doc) {
-    // if only one result, wrap it in an array
-    if (!Base.elementHasMany(doc[this._plural]))
-      doc[this._plural][this._singular] = [doc[this._plural][this._singular]];
-    
-    var results = doc[this._plural][this._singular].map(function(elem) {
-      return this.build(this.dataFromTree(elem));
-    }.bind(this));
-    
-    // This is better than requiring the controller to support a "limit" parameter
-    if (id == "first")
-      return results[0];
-      
-    return results; 
-  }.bind(this);
-  
-  findOneTransform = function(doc) {
-    data = this.dataFromTree(doc[this._singular]);
-    return this.build(data);
-  }.bind(this);
-      
-  if (id == "first" || id == "all") {
-    var url = this.plural_url();
-    return this._request(findAllTransform, url, callback);
-  }
-  else {
-    if (isNaN(parseInt(id))) return null;
-    url = this.singular_url(id);
-    return this._request(findOneTransform, url, callback);
-  }
-};
-
-// Reload
-Base.prototype.reload = function() {
-  if (this.id) {
-    var copy = this.find(this.id);
-    for (var i=0; i<copy.attributes.length; i++)
-      this.setAttribute(copy.attributes[i], copy[copy.attributes[i]]);
-    for (var i=0; i<copy.associations.length; i++)
-      this.setAssociation(copy.associations[i], copy[copy.associations[i]]);
-  }
-  return this;
-}
-
-// Converts the XML tree returned from an errors object into an array of error messages
-Base.prototype.errorsFromTree = function(elements) {
-
-  var errors = [];
-  if (typeof(elements.error) == "string")
-    elements.error = [elements.error];
-  
-  elements.error.each(function(value, index) {
-    errors.push(value);
-  });
-  
-  return errors;
-}
-
-// Sets errors with an array.  Could be extended at some point to include breaking error messages into pairs (attribute, msg).
-Base.prototype.setErrors = function(errors) {
-  this.errors = errors;
-}
-
-// New (no Save)
-Base.prototype.build = function(data, name, prefix, singular, plural) {
-  var base;
-  if (name)
-    base = new Base(name, prefix, singular, plural)
-  else
-    base = new Base(this._name, this._prefix, this._singular, this._plural);
-  
-  base.setData(data);
-  return base;
-};
-Base.prototype._new = Base.prototype.build;
-
-// Create (New + Save)
-Base.prototype.create = function(attributes) {
-  var base = this.build(attributes);
-  base.save();
-  
-  return base;
-};
-
-// Destroy
-// If not given an ID, destroys itself if it has an ID.  If given an ID, destroys that record.
-Base.prototype.destroy = function(given_id) {
-  var id = given_id || this.id;
-  if (!id) return false;
-  
-  var req = new Ajax.Request(this.singular_url(id), {
-    method: "delete",
-    asynchronous: false
-  });
-  
-  if (req.transport.status == 200) {
-    if (!given_id || this.id == given_id)
-      this.id = null;
-    return this;
-  }
-  else 
-    return false;
-};
-
-// Converts the XML tree returned from a single object into a hash of attribute values
-Base.prototype.dataFromTree = function(elements) {
-  var attributes = {}
-  for (var attr in elements) {
-    // pull out the value
-    var value;
-    if (elements[attr]["#text"])
-      value = elements[attr]["#text"];
-    else
-      value = elements[attr];
-    
-    // handle scalars
-    if (typeof(value) == "string") {
-      // perform any useful type transformations
-      if (elements[attr]["@type"] == "integer")
-        value = parseInt(value);
-      else if (elements[attr]["@type"] == "boolean")
-        value = (value == "true");
-      else if (elements[attr]["@type"] == "datetime") {
-        date = Date.parse(value);
-        if (!isNaN(date)) value = date; // date will be NaN if jester_utils isn't loaded
-      }
-    }
-    // handle arrays (associations)
-    else {
-      var relation = value; // rename for clarity in the context of an association
-      
-      // first, detect if it's has_one/belongs_to, or has_many
-      var i = 0;
-      var singular = null;
-      var has_many = false;
-      for (var val in relation) {
-        if (i == 0)
-          singular = val;
-        i += 1;
-      }
-      
-      
-      // has_many
-      if (relation[singular] && typeof(relation[singular]) == "object" && i == 1) {
-        var value = [];
-        var plural = attr;
-        var name = singular.capitalize();
-        
-        // force array
-        if (!(elements[plural][singular].length > 0))
-          elements[plural][singular] = [elements[plural][singular]];
-        
-        elements[plural][singular].each(function(single) {
-          value.push(this.build(this.dataFromTree(single), name, this._prefix, singular, plural));
-        }.bind(this));
-      }
-      // has_one or belongs_to
-      else {
-        singular = attr;
-        var name = singular.capitalize();
-        value = this.build(this.dataFromTree(value), name, this._prefix, singular);
-      }
-    }
-    
-    // transform attribute name if needed
-    attribute = attr.replace(/-/, "_");
-    attributes[attribute] = value;
-  }
-  
-  return attributes;
-};
-
-
-// Sets all attributes and associations at once
-// Deciding between the two on whether the attribute is a complex object or a scalar
-Base.prototype.setData = function(data) {
-  this.clear();
-  for (var attr in data) {
-    if (typeof(data[attr]) == "object")
-      this.setAssociation(attr, data[attr]);
-    else
-      this.setAttribute(attr, data[attr]);
-  }
-};
-
-// Set attributes
-// Force this array to be treated as attributes
-Base.prototype.setAttributes = function(attributes) {
-  this.clearAttributes();
-  for (var attr in attributes)
-    this.setAttribute(attr, attributes[attr])
-}
-
-// Set associations
-// Force this array to be treated as associations
-Base.prototype.setAssociations = function(associations) {
-  this.clearAssociations();
-  for (var assoc in associations)
-    this.setAssociation(assoc, associations[assoc])
-}
-    
-// set a property accessor for an attribute
-// I don't see any issue with calling this publicly, either
-Base.prototype.setAttribute = function(attribute, value) {  
-  this[attribute] = value;
-  if (!(this.attributes.include(attribute)))
-    this.attributes.push(attribute);  
-};
-
-// sets a property accessor for an association
-Base.prototype.setAssociation = function(association, value) {
-  this[association] = value;
-  if (!(this.associations.include(attribute)))
-    this.associations.push(attribute);
-}
-
-// clear the data
-Base.prototype.clear = function() {
-  this.clearAttributes();
-  this.clearAssociations();
-}
-
-// clear only attributes
-Base.prototype.clearAttributes = function() {
-  for (var i=0; i<this.attributes.length; i++)
-    this[this.attributes[i]] = null;
-  this.attributes = [];
-}
-
-// clear only associations
-Base.prototype.clearAssociations = function() {
-  for (var i=0; i<this.associations.length; i++)
-    this[this.associations[i]] = null;
-  this.associations = [];
-}
-
-
-
-// Save (Create and Update)
-Base.prototype.save = function() {
-
-  // reset errors
-  this.setErrors([]);
-
-  var url = null;
-  var method = null;
-  
-  // distinguish between create and update
-  if (this.new_record()) {
-    url = this.plural_url();
-    method = "post";
-  }
-  else {
-    url = this.singular_url();
-    method = "put";
-  }
-  
-  // collect params
-  var params = {};
-  (this.attributes).each(function(value, i) {
-    params[this._singular + "[" + value + "]"] = this[value];
-  }.bind(this));
-  
-  // send the request
-  var req = new Ajax.Request(url, {
-    parameters: params,
-    method: method,
-    asynchronous: false
-  });
-  
-  var status = req.transport.status;
-  var saved = false;
-  
-  // create response
-  if (this.new_record()) {
-    if (status == 201) {
-      loc = req.getHeader("location");
-      if (loc) {
-        id = loc.match(/\/([^\/]*?)(\.\w+)?$/)[1];
-        if (id) {
-          this.id = parseInt(id);
-          saved = true;
-        }
-      }
-    }
-    // check for errors
-    else if (status == 200) {
-      if (req.transport.responseText) {
-        var doc = this._tree.parseXML(req.transport.responseText);
-        if (doc.errors)
-          this.setErrors(this.errorsFromTree(doc.errors));
-      }
-    }
-  }
-  // update response
-  else {
-    if (status == 200) {
-      saved = true;
-      // check for errors
-      if (req.transport.responseText) {
-        var doc = this._tree.parseXML(req.transport.responseText);
-        if (doc.errors) {
-          this.setErrors(this.errorsFromTree(doc.errors));
-          saved = false;
-        }
-      }
-    }
-  }
-  
-  // return whether the save succeeded
-  return saved;
-};
-
-// handle all http requests
-// Currently uses ObjTree's ParseHTTP, which in turn uses Prototype
-Base.prototype._request = function(callback, url, user_callback) {
-  if (user_callback) {
-    return this._tree.parseHTTP(url, {}, function(doc) {
-      user_callback(callback(doc))
-    });
-  }
-  else
-    return callback(this._tree.parseHTTP(url, {}));
-};
 
 // Returns true if the element has more objects beneath it, or just 1 or more attributes.
 // It's not perfect, this would mess up if an object had only one attribute, and it was an array.
@@ -438,6 +108,325 @@ Base.elementHasMany = function(element) {
   
   return (element[singular] && typeof(element[singular]) == "object" && element[singular].length != null && i == 1);
 }
+
+extend(Base.prototype, {
+
+  // helper URLs
+  singular_url : function(id) {return ((id || this.id) ? this._prefix + "/" + this._plural + "/" + (id || this.id) + ".xml" : "");},
+  plural_url : function() {return this._prefix + "/" + this._plural + ".xml";},
+  
+  new_record : function() {return !(this.id);},
+  valid : function() {return ! this.errors.any();},
+  
+  find : function(id, callback) {
+    findAllTransform = function(doc) {
+      // if only one result, wrap it in an array
+      if (!Base.elementHasMany(doc[this._plural]))
+        doc[this._plural][this._singular] = [doc[this._plural][this._singular]];
+      
+      var results = doc[this._plural][this._singular].map(function(elem) {
+        return this.build(this.dataFromTree(elem));
+      }.bind(this));
+      
+      // This is better than requiring the controller to support a "limit" parameter
+      if (id == "first")
+        return results[0];
+        
+      return results; 
+    }.bind(this);
+    
+    findOneTransform = function(doc) {
+      data = this.dataFromTree(doc[this._singular]);
+      return this.build(data);
+    }.bind(this);
+        
+    if (id == "first" || id == "all") {
+      var url = this.plural_url();
+      return this._request(findAllTransform, url, callback);
+    }
+    else {
+      if (isNaN(parseInt(id))) return null;
+      url = this.singular_url(id);
+      return this._request(findOneTransform, url, callback);
+    }
+  },
+  
+  reload : function() {
+    if (this.id) {
+      var copy = this.find(this.id);
+      for (var i=0; i<copy.attributes.length; i++)
+        this.setAttribute(copy.attributes[i], copy[copy.attributes[i]]);
+      for (var i=0; i<copy.associations.length; i++)
+        this.setAssociation(copy.associations[i], copy[copy.associations[i]]);
+    }
+    return this;
+  },
+  
+  // This function would be named "new", if 
+  build : function(data, name, prefix, singular, plural) {
+    var base;
+    if (name)
+      base = new Base(name, prefix, singular, plural)
+    else
+      base = new Base(this._name, this._prefix, this._singular, this._plural);
+    
+    base.setData(data);
+    return base;
+  },
+  
+  create : function(attributes) {
+    var base = this.build(attributes);
+    base.save();
+    
+    return base;
+  },
+  
+  // If not given an ID, destroys itself, if it has an ID.  If given an ID, destroys that record.
+  destroy : function(given_id) {
+    var id = given_id || this.id;
+    if (!id) return false;
+    
+    var req = new Ajax.Request(this.singular_url(id), {
+      method: "delete",
+      asynchronous: false
+    });
+    
+    if (req.transport.status == 200) {
+      if (!given_id || this.id == given_id)
+        this.id = null;
+      return this;
+    }
+    else 
+      return false;
+  },
+  
+  save : function() {
+  
+    // reset errors
+    this.setErrors([]);
+  
+    var url = null;
+    var method = null;
+    
+    // distinguish between create and update
+    if (this.new_record()) {
+      url = this.plural_url();
+      method = "post";
+    }
+    else {
+      url = this.singular_url();
+      method = "put";
+    }
+    
+    // collect params
+    var params = {};
+    (this.attributes).each(function(value, i) {
+      params[this._singular + "[" + value + "]"] = this[value];
+    }.bind(this));
+    
+    // send the request
+    var req = new Ajax.Request(url, {
+      parameters: params,
+      method: method,
+      asynchronous: false
+    });
+    
+    var status = req.transport.status;
+    var saved = false;
+    
+    // create response
+    if (this.new_record()) {
+      if (status == 201) {
+        loc = req.getHeader("location");
+        if (loc) {
+          id = loc.match(/\/([^\/]*?)(\.\w+)?$/)[1];
+          if (id) {
+            this.id = parseInt(id);
+            saved = true;
+          }
+        }
+      }
+      // check for errors
+      else if (status == 200) {
+        if (req.transport.responseText) {
+          var doc = this._tree.parseXML(req.transport.responseText);
+          if (doc.errors)
+            this.setErrors(this.errorsFromTree(doc.errors));
+        }
+      }
+    }
+    // update response
+    else {
+      if (status == 200) {
+        saved = true;
+        // check for errors
+        if (req.transport.responseText) {
+          var doc = this._tree.parseXML(req.transport.responseText);
+          if (doc.errors) {
+            this.setErrors(this.errorsFromTree(doc.errors));
+            saved = false;
+          }
+        }
+      }
+    }
+    
+    return saved;
+  },
+  
+  errorsFromTree : function(elements) {
+  
+    var errors = [];
+    if (typeof(elements.error) == "string")
+      elements.error = [elements.error];
+    
+    elements.error.each(function(value, index) {
+      errors.push(value);
+    });
+    
+    return errors;
+  },
+  
+  // Sets errors with an array.  Could be extended at some point to include breaking error messages into pairs (attribute, msg).
+  setErrors : function(errors) {
+    this.errors = errors;
+  },
+  
+  // Converts the XML tree returned from a single object into a hash of attribute values
+  dataFromTree : function(elements) {
+    var attributes = {}
+    for (var attr in elements) {
+      // pull out the value
+      var value;
+      if (elements[attr]["#text"])
+        value = elements[attr]["#text"];
+      else
+        value = elements[attr];
+      
+      // handle scalars
+      if (typeof(value) == "string") {
+        // perform any useful type transformations
+        if (elements[attr]["@type"] == "integer")
+          value = parseInt(value);
+        else if (elements[attr]["@type"] == "boolean")
+          value = (value == "true");
+        else if (elements[attr]["@type"] == "datetime") {
+          date = Date.parse(value);
+          if (!isNaN(date)) value = date; // date will be NaN if jester_utils isn't loaded
+        }
+      }
+      // handle arrays (associations)
+      else {
+        var relation = value; // rename for clarity in the context of an association
+        
+        // first, detect if it's has_one/belongs_to, or has_many
+        var i = 0;
+        var singular = null;
+        var has_many = false;
+        for (var val in relation) {
+          if (i == 0)
+            singular = val;
+          i += 1;
+        }
+        
+        
+        // has_many
+        if (relation[singular] && typeof(relation[singular]) == "object" && i == 1) {
+          var value = [];
+          var plural = attr;
+          var name = singular.capitalize();
+          
+          // force array
+          if (!(elements[plural][singular].length > 0))
+            elements[plural][singular] = [elements[plural][singular]];
+          
+          elements[plural][singular].each(function(single) {
+            value.push(this.build(this.dataFromTree(single), name, this._prefix, singular, plural));
+          }.bind(this));
+        }
+        // has_one or belongs_to
+        else {
+          singular = attr;
+          var name = singular.capitalize();
+          value = this.build(this.dataFromTree(value), name, this._prefix, singular);
+        }
+      }
+      
+      // transform attribute name if needed
+      attribute = attr.replace(/-/, "_");
+      attributes[attribute] = value;
+    }
+    
+    return attributes;
+  },
+  
+  
+  // Sets all attributes and associations at once
+  // Deciding between the two on whether the attribute is a complex object or a scalar
+  setData : function(data) {
+    this.clear();
+    for (var attr in data) {
+      if (typeof(data[attr]) == "object")
+        this.setAssociation(attr, data[attr]);
+      else
+        this.setAttribute(attr, data[attr]);
+    }
+  },
+  
+  // Set attributes
+  // Force this array to be treated as attributes
+  setAttributes : function(attributes) {
+    this.clearAttributes();
+    for (var attr in attributes)
+      this.setAttribute(attr, attributes[attr])
+  },
+  
+  setAssociations : function(associations) {
+    this.clearAssociations();
+    for (var assoc in associations)
+      this.setAssociation(assoc, associations[assoc])
+  },
+      
+  setAttribute : function(attribute, value) {  
+    this[attribute] = value;
+    if (!(this.attributes.include(attribute)))
+      this.attributes.push(attribute);  
+  },
+  
+  setAssociation : function(association, value) {
+    this[association] = value;
+    if (!(this.associations.include(attribute)))
+      this.associations.push(attribute);
+  },
+  
+  clear : function() {
+    this.clearAttributes();
+    this.clearAssociations();
+  },
+  
+  clearAttributes : function() {
+    for (var i=0; i<this.attributes.length; i++)
+      this[this.attributes[i]] = null;
+    this.attributes = [];
+  },
+  
+  clearAssociations : function() {
+    for (var i=0; i<this.associations.length; i++)
+      this[this.associations[i]] = null;
+    this.associations = [];
+  },
+  
+  
+  _request : function(callback, url, user_callback) {
+    if (user_callback) {
+      return this._tree.parseHTTP(url, {}, function(doc) {
+        user_callback(callback(doc))
+      });
+    }
+    else
+      return callback(this._tree.parseHTTP(url, {}));
+  },
+
+});
 
 /* 
   Inflector library, contributed graciously to Jester by Ryan Schuft.  This is a port of Rails' built in pluralization.
